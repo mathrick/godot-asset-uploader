98a92f31e88b5da33078e80677293f9fe3b8d3ba
Maciej Katafiasz <mathrick@gmail.com>
2024-09-16 19:48:00 -0700

    Add changelog injection

--- godot_asset_uploader/main.py
+++ godot_asset_uploader/main.py
@@ -4,19 +4,24 @@ from . import vcs, config
 from .markdown import Renderer, Document
 
 @click.command()
-@click.option("--readme", default="README.md")
-@click.option("--changelog", default="CHANGELOG.md")
+@click.option("--readme", default="README.md", help="Location of README file, relative to project root")
+@click.option("--changelog", default="CHANGELOG.md", help="Location of changelog file, relative to project root")
 @click.argument("root", default=".")
 def main(root, readme, changelog):
     """Automatically upload or update an asset in Godot Asset Library
-based on the project repository"""
+based on the project repository.
+
+ROOT should be the root of the project, meaning a directory containing
+the file 'gdasset.ini', or a VCS repository (currently, only Git is
+supported). If not specified, it will be determined automatically,
+starting at the current directory."""
     project_root = vcs.get_project_root(root)
     cfg = config.Config(
         readme=project_root / readme,
         changelog=changelog and project_root / changelog
     )
     with open(readme) as input:
-        with Renderer(cfg, max_line_length=200) as renderer:
+        with Renderer(cfg, max_line_length=None) as renderer:
             rendered = renderer.render(Document(input))
             print(rendered)
 
--- godot_asset_uploader/markdown.py
+++ godot_asset_uploader/markdown.py
@@ -1,28 +1,35 @@
 import re
 
 from mistletoe import Document
-from mistletoe.block_token import BlockToken
+from mistletoe.block_token import BlockToken, List, Paragraph
 from mistletoe.span_token import SpanToken
 from mistletoe.markdown_renderer import MarkdownRenderer
 from mistletoe.ast_renderer import AstRenderer
 
+from . import errors as err
+
 class MetaItem(SpanToken):
-    def __init__(self, match):
-        self.tag, *value = match.split(":", 1)
-        self.value = value[0].lstrip() if value else None
-            
+    def __init__(self, matches):
+        header, *attrs = matches
+        tag, *value = header.split(":", 1)
+        self.tag = tag.lower()
+        self.value = value[0].strip() if value else None
+        self.attrs = {k.lower(): v.strip()
+                      for attr in attrs
+                      for k, v in [attr.split(":", 1)]}
+
     @property
     def content(self):
         return ": ".join([self.tag] + ([self.value] if self.value is not None else []))
 
 
 class MetaComment(BlockToken):
-    START_REGEX = re.compile("^[^<]*<!--- (.*) (-->)?")
+    START_REGEX = re.compile("^[^<]*<!--- (.*)( -->)?")
     END_REGEX = re.compile("^(.*) -->")
 
     def __init__(self, match):
-        self.children = [MetaItem(m) for m in match]
-    
+        self.children = [MetaItem(match)]
+
     @property
     def content(self):
         return "<!--- {} -->".format('\n'.join([c.content for c in self.children]))
@@ -48,12 +55,44 @@ class MetaComment(BlockToken):
         return buf
 
 
-class Renderer(AstRenderer):
+class Renderer(MarkdownRenderer):
     def __init__(self, config, **kwargs):
         self.config = config
+        self.suppress = []
         super().__init__(MetaComment, **kwargs)
 
+    def render(self, token):
+        result = ""
+        try:
+            if not self.suppress:
+                return super().render(token)
+        finally:
+            if token in self.suppress:
+                self.suppress.remove(token)
+
     def render_meta_comment(self, token, max_line_length=None):
+        return [self.render_meta_item(c) for c in token.children]
+
+    def render_meta_item(self, token, max_line_length=None):
         if token.tag == "changelog":
-            
-        return [f"!!! {token.content} !!!"]
+            if not self.config.changelog.exists():
+                raise err.GdAssetError(f"Changelog file {config.changelog} not found")
+            with self.config.changelog.open() as changelog_file:
+                changelog = None
+                for child in Document(changelog_file).children:
+                    if isinstance(child, List):
+                        changelog = child
+                        break
+                else:
+                    raise GdAssetError("Changelog file {config.changelog} does not contain a list")
+
+                to_render = [Paragraph([token.attrs["heading"] + ":"])] if "heading" in token.attrs else []
+
+                if token.value:
+                    changelog.children = changelog.children[:int(token.value)]
+
+                to_render.append(changelog)
+                token.parent.children = to_render
+                return "".join([self.render(x) for x in to_render])
+
+        raise err.GdAssetError(f"Unsupported metadata type: '{token.tag}'")
